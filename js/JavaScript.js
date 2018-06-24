f2340bdb3e252953212d1b35cc46b4d94d0a8fc4
Ariel Peralta <nuevosvientos75@yahoo.com.ar>
2018-06-24 12:41:25 -0300

    terminadooo

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,4 +1,4 @@
-
+ 
 function editarEstudiante(codigo) {
   var estudiante;
   for (var i = 0; i < localStorage.length; i++) {
@@ -29,13 +29,13 @@ function listarEstudiantes() {
   for (var i = 0; i < localStorage.length; i++) {
     var clave = localStorage.key(i);
     var estudiante = $.parseJSON(localStorage.getItem(clave));
-
+      
     tabla += '<tr>';
     tabla += '<td>' + estudiante.codigo + '</td>';
     tabla += '<td>' + estudiante.nombre + '</td>';
     tabla += '<td>' + estudiante.nota + '</td>';
-    tabla += '<td><button onclick="editarEstudiante(\'' + estudiante.codigo + '\');">Editar</button></td>';
-    tabla += '<td><button onclick="eliminarEstudiante(\'' + estudiante.codigo + '\');">Eliminar</button></td>';
+    tabla += '<td><button id="editar" onclick="editarEstudiante(\'' + estudiante.codigo + '\');">Editar</button></td>';
+    tabla += '<td><button id="eliminar" onclick="eliminarEstudiante(\'' + estudiante.codigo + '\');">Eliminar</button></td>';
     tabla += '</tr>';
   }
   tabla += '</table>';
@@ -58,6 +58,7 @@ $(document).ready(function() {
   $("#codigo").val(contador);
 
   $("#registrarestudiante").click(function() {
+      
     var codigo = $("#codigo").val();
     var nombre = $("#nombre").val();
     var nota = $("#nota").val();
@@ -66,19 +67,16 @@ $(document).ready(function() {
       codigo: codigo,
       nombre: nombre,
       nota: nota
+        
     };
 
     localStorage.setItem(codigo, JSON.stringify(estudiante));
     contador = localStorage.length + 1;
-
+    
     listarEstudiantes();
     restablecer();
 
   });
-
-  //$("#boton2").click(function() {
-  //  restablecer(9);
-//  });
 
   function restablecer() {
     $("#codigo").val(contador);
@@ -90,6 +88,7 @@ $(document).ready(function() {
   $("#codigo").val();
 
 });
+
 
 $("#mipromedio").click(function() {
       var acumulador = 0.0,
